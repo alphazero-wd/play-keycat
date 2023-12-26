eab7d9dc0a776284126e9fc47fa5a02b47986265
alphazero-wd <alphazero25811@gmail.com>
2023-12-26 18:18:32 +0700

    fix not testing form res �️

--- client/features/auth/login/login-form.spec.tsx
+++ client/features/auth/login/login-form.spec.tsx
@@ -1,5 +1,5 @@
 import { Alert } from "@/features/ui/alert";
-import { render, screen } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { HttpResponse, http } from "msw";
 import { setupServer } from "msw/node";
@@ -10,7 +10,6 @@ const handlers = [
     return HttpResponse.json({ username: "bob" });
   }),
 ];
-
 const server = setupServer(...handlers);
 beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
 afterEach(() => server.resetHandlers());
@@ -23,7 +22,7 @@ test("should display login form", async () => {
 
 describe("validation is not successful", () => {
   it("should show errors if email and password fields are empty", async () => {
-    render(<LoginForm />);
+    const screen = render(<LoginForm />);
     const loginButton = screen.getByText(/log in/i);
     await userEvent.click(loginButton);
     const emailErrorMessage = await screen.findByText(/email is required/i);
@@ -34,7 +33,7 @@ describe("validation is not successful", () => {
   });
 
   it("should show an error if email is not valid", async () => {
-    render(<LoginForm />);
+    const screen = render(<LoginForm />);
     const emailInput = screen.getByLabelText(/email/i);
     const loginButton = screen.getByText(/log in/i);
     await userEvent.type(emailInput, "abc@a");
@@ -44,75 +43,116 @@ describe("validation is not successful", () => {
   });
 });
 
-describe("validation is successful, but login is not", () => {
-  it("should display loading", async () => {
-    render(<LoginForm />);
+describe("validation is successful", () => {
+  it("should display loading and disable the button until response is received", async () => {
+    const screen = render(<LoginForm />);
     const emailInput = screen.getByLabelText(/email/i);
     const passwordInput = screen.getByLabelText(/password/i);
     const loginButton = screen.getByText(/log in/i);
     await userEvent.type(emailInput, "bob@bob.com");
     await userEvent.type(passwordInput, "123");
     await userEvent.click(loginButton);
-    expect(emailInput).toHaveTextContent("");
-    expect(passwordInput).toHaveTextContent("");
     expect(loginButton).toHaveTextContent(/logging in.../i);
+    expect(loginButton).toBeDisabled();
+
+    await waitFor(
+      () => {
+        expect(loginButton).toHaveTextContent(/log in/i);
+        expect(loginButton).not.toBeDisabled();
+      },
+      { timeout: 2000 }, // 1 second delay + time for promise to be fulfilled > 1s
+    );
   });
 
-  it("should display error alert if email or password is incorrect", async () => {
-    server.use(
-      http.post("/auth/login", () => {
-        return new HttpResponse(null, { status: 400 });
-      }),
-    );
-    render(
-      <>
-        <Alert />
-        <LoginForm />
-      </>,
-    );
-    const emailInput = screen.getByLabelText(/email/i);
-    const passwordInput = screen.getByLabelText(/password/i);
-    const loginButton = screen.getByText(/log in/i);
-    await userEvent.type(emailInput, "bob@bob.com");
-    await userEvent.type(passwordInput, "123");
-    await userEvent.click(loginButton);
-    expect(await screen.findByRole("alert")).toBeInTheDocument();
-    expect(await screen.findByText(/error/i)).toBeInTheDocument();
-    const alertContent = await screen.findByText(
-      /wrong email or password provided/i,
-    );
-    expect(alertContent).toBeInTheDocument();
-  });
-});
+  describe("and log in is not successful", () => {
+    it("should display error alert if email or password is incorrect", async () => {
+      const components = (
+        <>
+          <Alert />
+          <LoginForm />
+        </>
+      );
+      server.use(
+        http.post("/auth/login", () => {
+          return new HttpResponse(null, { status: 400 });
+        }),
+      );
+      const screen = render(components);
+      const emailInput = screen.getByLabelText(/email/i);
+      const passwordInput = screen.getByLabelText(/password/i);
+      const loginButton = screen.getByText(/log in/i);
+      await userEvent.type(emailInput, "bob@bob.com");
+      await userEvent.type(passwordInput, "123");
+      await userEvent.click(loginButton);
+      expect(await screen.findByRole("alert")).toBeInTheDocument();
+      expect(await screen.findByText(/error/i)).toBeInTheDocument();
+      const alertContent = await screen.findByText(
+        /wrong email or password provided/i,
+      );
+      expect(alertContent).toBeInTheDocument();
+    });
 
-describe("validation and login are successful", () => {
-  it("should reset form and display loading if login successfully", async () => {
-    render(<LoginForm />);
-    const emailInput = screen.getByLabelText(/email/i);
-    const passwordInput = screen.getByLabelText(/password/i);
-    const loginButton = screen.getByText(/log in/i);
-    await userEvent.type(emailInput, "bob@bob.com");
-    await userEvent.type(passwordInput, "123");
-    await userEvent.click(loginButton);
-    expect(emailInput).toHaveTextContent("");
-    expect(passwordInput).toHaveTextContent("");
-    expect(loginButton).toHaveTextContent(/logging in.../i);
+    it("should display error alert if some unexpected error occur", async () => {
+      const components = (
+        <>
+          <Alert />
+          <LoginForm />
+        </>
+      );
+      server.use(
+        http.post("/auth/login", () => {
+          return new HttpResponse(null, { status: 500 });
+        }),
+      );
+      const screen = render(components);
+      const emailInput = screen.getByLabelText(/email/i);
+      const passwordInput = screen.getByLabelText(/password/i);
+      const loginButton = screen.getByText(/log in/i);
+      await userEvent.type(emailInput, "bob@bob.com");
+      await userEvent.type(passwordInput, "123");
+      await userEvent.click(loginButton);
+      expect(await screen.findByRole("alert")).toBeInTheDocument();
+      expect(await screen.findByText(/error/i)).toBeInTheDocument();
+      const alertContent = await screen.findByText(/something went wrong/i);
+      expect(alertContent).toBeInTheDocument();
+    });
   });
-  it("should display success alert", async () => {
-    render(
-      <>
-        <Alert />
-        <LoginForm />
-      </>,
-    );
-    const emailInput = screen.getByLabelText(/email/i);
-    const passwordInput = screen.getByLabelText(/password/i);
-    const loginButton = screen.getByText(/log in/i);
-    await userEvent.type(emailInput, "bob@bob.com");
-    await userEvent.type(passwordInput, "123");
-    await userEvent.click(loginButton);
-    expect(await screen.findByRole("alert")).toBeInTheDocument();
-    expect(await screen.findByText(/success/i)).toBeInTheDocument();
-    expect(await screen.findByText(/welcome back, bob/i)).toBeInTheDocument();
+
+  describe("and login are successful", () => {
+    it("should reset form if login successfully", async () => {
+      const screen = render(<LoginForm />);
+      const emailInput = screen.getByLabelText(/email/i);
+      const passwordInput = screen.getByLabelText(/password/i);
+      const loginButton = screen.getByText(/log in/i);
+      await userEvent.type(emailInput, "bob@bob.com");
+      await userEvent.type(passwordInput, "123");
+      await userEvent.click(loginButton);
+
+      await waitFor(
+        () => {
+          expect(emailInput).toHaveValue("");
+          expect(passwordInput).toHaveValue("");
+        },
+        { timeout: 2000 },
+      );
+    });
+    it("should display success alert", async () => {
+      const components = (
+        <>
+          <Alert />
+          <LoginForm />
+        </>
+      );
+      const screen = render(components);
+      const emailInput = screen.getByLabelText(/email/i);
+      const passwordInput = screen.getByLabelText(/password/i);
+      const loginButton = screen.getByText(/log in/i);
+      await userEvent.type(emailInput, "bob@bob.com");
+      await userEvent.type(passwordInput, "123");
+      await userEvent.click(loginButton);
+      expect(await screen.findByRole("alert")).toBeInTheDocument();
+      expect(await screen.findByText(/success/i)).toBeInTheDocument();
+      expect(await screen.findByText(/welcome back, bob/i)).toBeInTheDocument();
+    });
   });
 });
